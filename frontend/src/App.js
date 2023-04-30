37c2b7e0c738772e9990f134f787adceb2f9ef6f
SoheilKhorram <soheil13261428@gmail.com>
2023-04-30 12:22:17 +0430

    connecting back to front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import axios from 'axios'
+
+const data = {
+    name: "so",
+    email: "ssad;nlq2"
+}
+
+axios.post("/test", data)
+    .then(response => {
+        console.log(response.data)
+    })
+    .catch(err => {
+        console.error(err)
+    })
 
 const App = () => {
     return (
-        <div>App</div>
+        <form method='POST' action='/post'>
+            <input type='text'></input>
+            <input type='submit'></input>
+        </form>
     )
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const app = require('express')();
+const app = require('express')()
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
 
-app.post("/post", (req, res) => 
-{
-console.log("Connected !");
-res.redirect("/");
-});
+console.log("Server start")
 
-app.listen(5000);
+const data = {
+    name: "!!!!!!!!!",
+    email: "***********"
+}
+
+app.post("/post", (req, res) => {
+    console.log("Connected !")
+    res.redirect("/")
+})
+
+app.post("/test", (req, res) => {
+    console.log(req.body)
+    res.send(data)
+})
+
+app.listen(5000)
