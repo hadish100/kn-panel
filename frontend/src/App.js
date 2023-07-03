6d82b819cfae47fcc364a68245fa6316529d8613
SoheilKhorram <soheil13261428@gmail.com>
2023-07-04 01:53:29 +0430

    some minor change for the properties

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,7 +51,7 @@ let users = [
 const App = () => {
     return (
         <>
-            <UsageStats activeUsers={10} totalUsers={549} dataUsage="1 GB" memoryUsage="256 MB" totalMemory="3.8 GB" />
+            <UsageStats activeUsers={10} totalUsers={549} dataUsage="1 GB" memoryUsage="256 MB" totalMemory="3.8 GB" allocableData="1 TB" />
             <UsersTable users={users} />
         </>
     )
