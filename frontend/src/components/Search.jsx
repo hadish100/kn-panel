cb58468cff0010290f66f72c68c57ac115a4fd79
SoheilKhorram <soheil13261428@gmail.com>
2023-07-05 03:06:38 +0430

    some minor change for the icons in search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -12,7 +12,7 @@ const Search = () => {
     return (
         <div className="search">
             <div className="search__icon">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="css-1o363pe"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 -2 24 24" stroke-width="1.65" stroke="currentColor" aria-hidden="true" class="css-1o363pe"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path></svg>
             </div>
             <input
                 type="text"
@@ -23,7 +23,7 @@ const Search = () => {
             {
                 search.length > 0 &&
                 <div className="search__icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" focusable="false" class="css-1o363pe"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.65" stroke="currentColor" aria-hidden="true" focusable="false" class="css-1o363pe"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
                 </div>
             }
         </div>
