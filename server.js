2370c1427d8054fce3718cab49bb68de056709ac
pouya <hadishhamraz@gmail.com>
2023-07-04 19:59:09 +0300

    test commit

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const app = require('express')()
-const { json } = require('body-parser')
+const app = require('express')();
+const { json } = require('body-parser');
 
-app.use(json())
+app.use(json());
 
-const data = {
-    name: "!!!!!!!!!",
-    email: "***********"
-}
 
-app.post("/test", (req, res) => {
+
+app.post("/test", (req, res) => 
+{
     console.log(req.body)
-    res.send(data)
 })
 
-app.listen(5000, () => {
-    console.log("Server is listening on port 5000")
+app.listen(5000, () => 
+{
+    console.log("--------------");
+    console.log("SERVER STARTED !");
+    console.log("--------------");
 })
