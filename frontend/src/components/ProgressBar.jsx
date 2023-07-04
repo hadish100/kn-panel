9f0e301e9f39cc1e316c77e01b864172218ae715
SoheilKhorram <soheil13261428@gmail.com>
2023-07-04 11:55:39 +0430

    working on the progress bar

---
+++ frontend/src/components/ProgressBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ProgressBar.css";
+
+const ProgressBar = ({ dataUsage, totalData, status }) => {
+    return (
+        <div className="progress-bar">
+            <div className={`progress-bar__filler--${status}`} style={{ width: `${dataUsage / totalData * 100}%` }}></div>
+        </div>
+    );
+}
+
+export default ProgressBar
