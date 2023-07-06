a0efbcd8042a659883bc2b01e24ed9794a5103bb
SoheilKhorram <soheil13261428@gmail.com>
2023-07-06 17:36:44 +0430

    styling the modal and its form

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -5,8 +5,9 @@ import "./Modal.css";
 
 const Modal = ({ children, onClose, actionBar }) => {
     return ReactDOM.createPortal(
-        <div className="modal__background" onClick={onClose}>
-            <div className="modal__content">
+        <div className="modal" onClick={onClose}>
+            <div className="modal__background" ></div>
+            <div className="modal__content" onClick={(e) => e.stopPropagation()}>
                 {children}
                 {actionBar}
             </div>
--- frontend/src/components/UsersTable.jsx
+++ frontend/src/components/UsersTable.jsx
@@ -6,6 +6,8 @@ import SubscriptionSection from "./SubscriptionSection";
 import Search from "./Search";
 import Button from "./Button";
 import Modal from "./Modal";
+import LeadingIcon from "./LeadingIcon";
+import { ReactComponent as AddUserIcon } from "../assets/add-user.svg";
 
 const UsersTable = ({ users }) => {
 
@@ -50,9 +52,28 @@ const UsersTable = ({ users }) => {
         <Button onClick={handleClose}>Save</Button>
     </div>
     const modal = <Modal onClose={handleClose} actionBar={actionBar} >
-        <p>
-            HEEEEEYYYY
-        </p>
+        <header className="modal__header">
+            <LeadingIcon>
+                <AddUserIcon />
+            </LeadingIcon>
+            <h1 className="modal__title">Create new user</h1>
+        </header>
+        <main className="modal__body">
+            <form className="modal__form">
+                <div className="modal__form__group">
+                    <label className="modal__form__label" htmlFor="username">Username</label>
+                    <input className="modal__form__input" type="text" id="username" name="username" />
+                </div>
+                <div className="modal__form__group">
+                    <label className="modal__form__label" htmlFor="dataLimit">Data Limit</label>
+                    <input className="modal__form__input" type="number" id="dataLimit" name="dataLimit" />
+                </div>
+                <div className="modal__form__group">
+                    <label className="modal__form__label" htmlFor="daysToExpire">Days To Expire</label>
+                    <input className="modal__form__input" type="number" id="daysToExpire" name="daysToExpire" />
+                </div>
+            </form>
+        </main>
     </Modal>
 
 
