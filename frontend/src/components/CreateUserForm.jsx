3d70fa53d017842d16207ef92660074e4e13a9c9
SoheilKhorram <soheil13261428@gmail.com>
2023-07-08 15:18:26 +0430

    adding some animation to the input of the form

--- frontend/src/components/CreateUserForm.jsx
+++ frontend/src/components/CreateUserForm.jsx
@@ -5,6 +5,8 @@ import LeadingIcon from "./LeadingIcon";
 import Button from "./Button";
 import { ReactComponent as AddUserIcon } from "../assets/add-user.svg";
 import { ReactComponent as XMarkIcon } from "../assets/x-mark.svg";
+import { motion } from "framer-motion"
+
 
 const CreateUserForm = ({ handleClose }) => {
     return (
@@ -20,24 +22,24 @@ const CreateUserForm = ({ handleClose }) => {
             </header>
             <main className="modal__body">
                 <form className="modal__form">
-                    <div className="modal__form__group">
+                    <motion.div className="modal__form__group" animate={{ x: 0, opacity: 1 }} initial={{ x: -40, opacity: 0 }}>
                         <label className="modal__form__label" htmlFor="username">Username</label>
                         <input className="modal__form__input" type="text" id="username" name="username" />
-                    </div>
-                    <div className="modal__form__group">
+                    </motion.div>
+                    <motion.div className="modal__form__group" animate={{ x: 0, opacity: 1 }} initial={{ x: -40, opacity: 0 }} transition={{ delay: 0.1 }}>
                         <label className="modal__form__label" htmlFor="dataLimit">Data Limit</label>
                         <input className="modal__form__input" type="number" id="dataLimit" name="dataLimit" />
-                    </div>
-                    <div className="modal__form__group">
+                    </motion.div>
+                    <motion.div className="modal__form__group" animate={{ x: 0, opacity: 1 }} initial={{ x: -40, opacity: 0 }} transition={{ delay: 0.2 }}>
                         <label className="modal__form__label" htmlFor="daysToExpire">Days To Expire</label>
                         <input className="modal__form__input" type="number" id="daysToExpire" name="daysToExpire" />
-                    </div>
+                    </motion.div>
                 </form>
             </main>
-            <footer className="modal__footer">
+            <motion.footer className="modal__footer" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
                 <Button className={"transparent"} onClick={handleClose}>Cancel</Button>
                 <Button className={"primary"} onClick={handleClose}>Create User</Button>
-            </footer>
+            </motion.footer>
         </Modal>
     )
 }
