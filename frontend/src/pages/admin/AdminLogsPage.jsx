1848e6653bea2470e01d0de09d88d31bf95048bf
SoheilKhorram <soheil13261428@gmail.com>
2023-07-24 23:31:41 +0430

    basic styling for the logs page

--- frontend/src/pages/admin/AdminLogsPage.jsx
+++ frontend/src/pages/admin/AdminLogsPage.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Search from '../../components/Search'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 import "./AdminLogsPage.css"
 
 const AdminLogsPage = () => {
+    const [date, setDate] = useState(dayjs());
+
     return (
-        <div>AdminLogsPage</div>
+        <div className="admin-log-page">
+            <div className="admin-log-page__filter">
+                <Search />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        value={date}
+                        onChange={newDate => setDate(newDate)}
+                        slotProps={{ textField: { size: 'small' } }}
+                    />
+                </LocalizationProvider>
+            </div>
+            <ul>
+
+            </ul>
+        </div>
+
     )
 }
 
