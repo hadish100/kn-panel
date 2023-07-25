a9432e00b05e8d7b041add856af0beb83c49ded3
SoheilKhorram <soheil13261428@gmail.com>
2023-07-26 00:51:26 +0430

    creating log items with basic styling and logic

---
+++ frontend/src/components/admin/LogsList.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+import Button from '../../components/Button'
+import { ReactComponent as ChevronDownIcon } from '../../assets/svg/chevron-down.svg'
+import { Accordion } from '@mui/material';
+
+const LogsList = ({ logs }) => {
+    const [expandedId, setExpandedId] = useState(null);
+
+    const handleClick = (id) => {
+        if (id === expandedId) {
+            setExpandedId(null);
+        } else {
+            setExpandedId(id)
+        }
+    }
+
+    return (
+        <ul className='logs'>
+            {logs.map((log) => (
+                <li className='log' key={log.id}>
+                    <div className={`log__text ${expandedId === log.id ? "expanded" : ""}`} style={{ maxHeight: expandedId === log.id ? "7.5rem" : "2.5rem" }}>
+                        {log.logText}
+                        <span
+                            className='chevron-icon'
+                            style={{ alignSelf: "center", marginLeft: "0.25rem", }}>
+                            <Button className='ghosted' onClick={() => handleClick(log.id)}>
+                                <ChevronDownIcon />
+                            </Button>
+                        </span>
+                    </div>
+                    <div className="log__date">{log.logDate}</div>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default LogsList
--- frontend/src/pages/admin/AdminLogsPage.jsx
+++ frontend/src/pages/admin/AdminLogsPage.jsx
@@ -5,7 +5,26 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
+import LogsList from '../../components/admin/LogsList';
 import "./AdminLogsPage.css"
+
+const logs = [
+    {
+        logText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        logDate: "Today at 12:30 PM",
+        id: 1
+    },
+    {
+        logText: "lorem;asdlnalsdnlandnkbfiwfjqwefbqebfbqefbjqw c wekl eq eclbdlbqfl qelqbefl qefblqbeflqbefbqlefblq e,efbqljfbejlb",
+        logDate: "Today at 2:30 AM",
+        id: 2
+    },
+    {
+        logText: "lorem;asdlnalsdnlandnkbfiwfjqwefbqebfbqefbjqw c wekl eq eclbdlbqfl qelqbefl qefblqbeflqbefbqlefblq e,efbqljfbejlb",
+        logDate: "Today at 2:30 AM",
+        id: 3
+    },
+]
 
 const AdminLogsPage = () => {
     const [date, setDate] = useState(dayjs());
@@ -22,9 +41,7 @@ const AdminLogsPage = () => {
                     />
                 </LocalizationProvider>
             </div>
-            <ul>
-
-            </ul>
+            <LogsList logs={logs} />
         </div>
 
     )
