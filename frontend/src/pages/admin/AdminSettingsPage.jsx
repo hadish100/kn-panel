41dad24ebc1f566796035e8fcf688fca4f8785a0
SoheilKhorram <soheil13261428@gmail.com>
2023-07-27 11:55:35 +0430

    disabling the settings form from auto completing

--- frontend/src/pages/admin/AdminSettingsPage.jsx
+++ frontend/src/pages/admin/AdminSettingsPage.jsx
@@ -5,25 +5,25 @@ import "./AdminSettingsPage.css"
 
 const AdminSettingsPage = () => {
     return (
-        <div className="settings-page">
+        <form autoComplete='off' className="settings-page">
             <div className="modal__form__group">
                 <label className="modal__form__label" htmlFor="username">Username</label>
-                <input className="modal__form__input" type="text" id="username" name="username" />
+                <input autoComplete='new-username' className="modal__form__input" type="text" id="username" name="username" />
             </div>
             <div className="flex gap-16">
                 <div className="modal__form__group">
                     <label className="modal__form__label" htmlFor="password">New Password</label>
-                    <input className="modal__form__input" type="password" id="password" name="password" />
+                    <input autoComplete='new-password' className="modal__form__input" type="password" id="password" name="password" />
                 </div>
                 <div className="modal__form__group">
                     <label className="modal__form__label" htmlFor="password">Repeat New Password</label>
-                    <input className="modal__form__input" type="password" id="password" name="password" />
+                    <input autoComplete='new-password' className="modal__form__input" type="password" id="password" name="password" />
                 </div>
             </div>
             <footer className="settings-page__footer">
                 <Button className="primary">Save</Button>
             </footer>
-        </div>
+        </form>
     )
 }
 
