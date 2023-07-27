4d8294f6048a017568bc737878e1d4213cb42dd4
SoheilKhorram <soheil13261428@gmail.com>
2023-07-27 11:54:59 +0430

    adding spin icon when logging in

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Login.css'
 import { ReactComponent as LoginIcon } from "../assets/svg/login.svg"
 import { ReactComponent as Logo } from "../assets/svg/logo.svg"
+import { ReactComponent as RefreshIcon } from "../assets/svg/refresh.svg"
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button'
 import axios from "axios"
@@ -13,23 +14,23 @@ var error_message = "Please check your username and password";
 
 const Login = ({ setIsLoggedIn, setLocation }) => {
     const [hasError, setHasError] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
     const navigate = useNavigate();
+
     const send_login_data = async (e) => {
         e.preventDefault();
+        setIsLoading(true)
         document.querySelectorAll(".login__btn")[0].disabled = true;
         document.querySelectorAll(".login__btn")[0].classList.add("login__btn__clicked");
         var username = e.target[0].value
         var password = e.target[1].value
         var res;
-        
 
-        try
-        {
-            res = await axios.post("/login", { username, password },{ timeout: 20000 });
+        try {
+            res = await axios.post("/login", { username, password }, { timeout: 20000 });
         }
 
-        catch(err)
-        {
+        catch (err) {
             res = {};
             res.data = "ERR";
             error_message = "server is not responding";
@@ -39,7 +40,7 @@ const Login = ({ setIsLoggedIn, setLocation }) => {
         if (res.data === "ERR") {
             setHasError(true)
             document.querySelectorAll(".login__btn")[0].disabled = false;
-            document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked"); 
+            document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked");
         }
 
         else {
@@ -50,10 +51,9 @@ const Login = ({ setIsLoggedIn, setLocation }) => {
                 sessionStorage.setItem("isLoggedIn", "true")
                 sessionStorage.setItem("access_token", res.data.access_token)
 
-                try
-                {
-                    var agents = (await axios.post("/get_agents", { access_token },{ timeout: 20000 })).data;
-                    var panels = (await axios.post("/get_panels", { access_token },{ timeout: 20000 })).data;
+                try {
+                    var agents = (await axios.post("/get_agents", { access_token }, { timeout: 20000 })).data;
+                    var panels = (await axios.post("/get_panels", { access_token }, { timeout: 20000 })).data;
                     console.log(agents);
                     console.log(panels);
                     sessionStorage.setItem("agents", JSON.stringify(agents));
@@ -62,14 +62,12 @@ const Login = ({ setIsLoggedIn, setLocation }) => {
                     navigate('/admin/panels');
                 }
 
-                catch(err)
-                {
+                catch (err) {
                     error_message = "server is not responding";
                     setHasError(true)
                     document.querySelectorAll(".login__btn")[0].disabled = false;
-                    document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked"); 
+                    document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked");
                 }
-
             }
 
             else {
@@ -77,27 +75,24 @@ const Login = ({ setIsLoggedIn, setLocation }) => {
                 sessionStorage.setItem("isLoggedIn", "true");
                 sessionStorage.setItem("access_token", res.data.access_token)
 
-                try
-                {
-                    var users = (await axios.post("/get_users", { access_token },{ timeout: 20000 })).data;
+                try {
+                    var users = (await axios.post("/get_users", { access_token }, { timeout: 20000 })).data;
                     console.log(users);
                     sessionStorage.setItem("users", JSON.stringify(users));
                     setLocation("/agent/users");
                     navigate('/agent/users');
                 }
 
-                catch(err)
-                {
+                catch (err) {
                     error_message = "server is not responding";
                     setHasError(true)
                     document.querySelectorAll(".login__btn")[0].disabled = false;
-                    document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked"); 
+                    document.querySelectorAll(".login__btn")[0].classList.remove("login__btn__clicked");
                 }
-
-
             }
-
         }
+
+        setIsLoading(false)
     }
 
     var errorCard = (
@@ -117,9 +112,9 @@ const Login = ({ setIsLoggedIn, setLocation }) => {
             <form className='login__form' action="" onSubmit={send_login_data}>
                 <input type="text" placeholder='Username' className="username" name="username" />
                 <input type="password" placeholder='Password' className="password" name="password" />
-                <Button className="primary login__btn">
-                    <LoginIcon />
-                    Login
+                <Button className={`primary login__btn ${isLoading ? 'spin' : ''}`} disabled={isLoading} >
+                    {isLoading ? <RefreshIcon /> : <LoginIcon />}
+                    {isLoading ? 'Logging in...' : 'Login'}
                 </Button>
             </form>
             {errorCard}
