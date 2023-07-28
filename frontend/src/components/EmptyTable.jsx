cec3d66556189ac89d4edfef32873688a2e8f36d
SoheilKhorram <soheil13261428@gmail.com>
2023-07-28 12:48:40 +0430

    small change for hovering effect for the empty table component

--- frontend/src/components/EmptyTable.jsx
+++ frontend/src/components/EmptyTable.jsx
@@ -6,13 +6,15 @@ import './EmptyTable.css'
 
 const EmptyTable = ({ tableType, colSpan, onCreateButton }) => {
     return (
-        <td colSpan={colSpan}>
-            <div className="empty-table">
-                <EmptyTableIcon />
-                <h6>There is no {tableType} added to the system</h6>
-                <Button onClick={onCreateButton} className="primary">Create {tableType}</Button>
-            </div>
-        </td>
+        <tr className='empty-row'>
+            <td colSpan={colSpan}>
+                <div className="empty-table">
+                    <EmptyTableIcon />
+                    <h6>There is no {tableType} added to the system</h6>
+                    <Button onClick={onCreateButton} className="primary">Create {tableType}</Button>
+                </div>
+            </td>
+        </tr>
     )
 }
 
