1645f3f53240367a0f7e6dfb7fb37198ae43f6b5
SoheilKhorram <soheil13261428@gmail.com>
2023-07-28 21:31:31 +0430

    creating agent settings page

--- frontend/src/pages/agent/AgentSettingsPage.jsx
+++ frontend/src/pages/agent/AgentSettingsPage.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
-import './AgentSettingsPage.css'
+import Button from '../../components/Button'
+import "../admin/AdminSettingsPage.css"
 
-const AgentSettingsPage = () => {
+const AdminSettingsPage = () => {
     return (
-        <div className='panel_body'>
-            <div>SettingsPage</div>
-        </div>
+        <form autoComplete='off' className="settings-page">
+            <div className="modal__form__group">
+                <label className="modal__form__label" htmlFor="username">Username</label>
+                <input autoComplete='new-username' className="modal__form__input" type="text" id="username" name="username" />
+            </div>
+            <div className="flex gap-16">
+                <div className="modal__form__group">
+                    <label className="modal__form__label" htmlFor="password">New Password</label>
+                    <input autoComplete='new-password' className="modal__form__input" type="password" id="password" name="password" />
+                </div>
+                <div className="modal__form__group">
+                    <label className="modal__form__label" htmlFor="password">Repeat New Password</label>
+                    <input autoComplete='new-password' className="modal__form__input" type="password" id="password" name="password" />
+                </div>
+            </div>
+            <footer className="settings-page__footer">
+                <Button className="primary">Save</Button>
+            </footer>
+        </form>
     )
 }
 
-export default AgentSettingsPage
+export default AdminSettingsPage
