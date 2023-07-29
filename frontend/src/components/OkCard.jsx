65551c7534b8da978d838392d96bcbbd6ff76ed8
pouya <hadishhamraz@gmail.com>
2023-07-29 18:45:01 +0300

    added ok card 2

---
+++ frontend/src/components/OkCard.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+import { motion, AnimatePresence } from 'framer-motion'
+import { ReactComponent as XMarkIcon } from '../assets/svg/x-mark.svg'
+import okIcon from '../assets/accept.png'
+import styels from "./ErrorCard.module.css"
+
+const ErrorCard = ({ setHasError, hasError, errorTitle, errorMessage }) => {
+    useEffect(() => {
+        const errorTimer = setTimeout(() => {
+            handleClose()
+        }, 5000)
+
+        return () => clearTimeout(errorTimer)
+    },)
+
+    const handleClose = () => {
+        setHasError(false)
+    }
+
+    return ReactDOM.createPortal(
+        <AnimatePresence>
+            {hasError && <motion.div
+                className={styels.okCard}
+                initial={{ x: "120%" }}
+                animate={{ x: 0 }}
+                exit={{ x: "120%" }}
+                transition={{ type: "spring", stiffness: 100, damping: 12 }}
+            >
+                <img src={okIcon} className={styels.okCardImg} />
+                <div className={styels.errorCard__divider}>
+                    <header className={styels.errorCard__header}>
+                        <h2 className={styels.errorCard__title}>{errorTitle}</h2>
+                        <XMarkIcon onClick={handleClose} />
+                    </header>
+                    <p className={styels.errorCard__message}>{errorMessage}</p>
+                </div>
+            </motion.div >}
+        </AnimatePresence>,
+        document.querySelector(".error-container")
+    )
+}
+
+export default ErrorCard
