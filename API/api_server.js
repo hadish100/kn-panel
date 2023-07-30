633861fff3251b9b92490917d7a03e35c207ce5e
pouya <hadishhamraz@gmail.com>
2023-07-30 16:30:12 +0300

    added login endpoint

--- API/api_server.js
+++ API/api_server.js
@@ -23,21 +23,35 @@ const uid = () => { return Math.floor(Math.random() * (999999999 - 100000000 + 1
 const insert_to_accounts = async (obj) => { await accounts.insertOne(obj);return "DONE"; }
 const get_accounts = async () => {const result = await accounts.find().toArray();return result;}
 const get_account = async (id) => {const result = await accounts.find({id}).toArray();return result[0];}
-const update_account = async (id,value) => {await accounts.updateOne({id:id},{$set:value},function(){});return "DONE";}
+const update_account = async (id,value) => {await accounts.updateOne({id},{$set:value},function(){});return "DONE";}
+
+const add_token = async (id) => 
+{
+    var expire = Math.floor(Date.now()/1000) + 3600;
+    var token = uid();
+    var obj = { token , expire };
+    await accounts.updateOne({id},{$push:{tokens:obj}},function(){});return token;
+}
 
 
 app.post("/api/login/" , async (req,res) =>
 {
+    const {username,password} = req.body;
+    const accounts = await get_accounts();
+    const account = accounts.filter(x => x.username == username && x.password == password)[0];
+
+    if(account)
+    {
+        var access_token = await add_token(account.id);
+        res.json({is_admin:account.is_admin,access_token});
+    }
+
+    else
+    {
+        res.status(400).send({message: 'NOT FOUND'});
+    }
 
 });
-
-
-
-
-
-
-
-
 
 
 
--- server.js
+++ server.js
@@ -91,15 +91,8 @@ app.post("/login", async (req, res) => {
     const { username, password } = req.body;
     try {
         var api_auth_res = (await axios.post(API_SERVER_URL + '/api/login/', { username, password }, { headers: { 'accept': 'application/json', 'Content-Type': 'application/json' } })).data;
-        access_token = 'Bearer ' + api_auth_res.access;
 
-        user_data =
-        {
-            access_token: access_token,
-            is_admin: api_auth_res.user.is_admin
-        };
-
-        res.send(user_data);
+        res.send(api_auth_res);
     }
 
     catch (err) {
