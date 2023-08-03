0ec494cbb7a1b45296fa67f16a5e5f0eb03545bd
pouya <hadishhamraz@gmail.com>
2023-08-04 00:03:13 +0300

    added 2 step verification to logout and reset usage

---
+++ frontend/src/components/admin/VerifyReset.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { ReactComponent as DeleteIcon } from "../../assets/svg/refreshWhite.svg"
+import { ReactComponent as XMarkIcon } from '../../assets/svg/x-mark.svg';
+import { motion,AnimatePresence } from 'framer-motion';
+import LeadingIcon from '../LeadingIcon';
+import Modal from '../Modal';
+import Button from '../Button';
+
+const VerifyDelete = ({ onClose, showForm, onDeleteItem }) => {
+
+ 
+    const formHeader = (
+        <header className="modal__header">
+            <LeadingIcon>
+                <DeleteIcon />
+            </LeadingIcon>
+            <h1 className="modal__title">Confirm Reset usage</h1>
+            <div className="close-icon" onClick={onClose}>
+                <XMarkIcon />
+            </div>
+        </header>
+    )
+
+
+    const formFooter = (
+        <motion.footer className="modal__footer" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <Button className="outlined wide_btn" onClick={onClose} > Cancel </Button>
+            <Button className="primary wide_btn" onClick={onDeleteItem} > Reset </Button>
+        </motion.footer>
+    )
+
+    return (
+        <AnimatePresence>
+        {showForm && (
+            <Modal v2="true" onClose={onClose}>
+                {formHeader}
+                {formFooter}
+            </Modal>
+        )}
+    </AnimatePresence>
+    )
+}
+
+export default VerifyDelete
