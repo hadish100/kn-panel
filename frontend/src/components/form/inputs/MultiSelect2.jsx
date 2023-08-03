f3415940cb1f513cb37a7556c4dd2f07467660f6
pouya <hadishhamraz@gmail.com>
2023-08-03 03:40:02 +0300

    forgot to add files

---
+++ frontend/src/components/form/inputs/MultiSelect2.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const agent = JSON.parse(sessionStorage.getItem("agent"));
+
+  return (
+    <Box sx={{ width:'100%' }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label"></InputLabel>
+        <Select sx={{height:34}}
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label=""
+          onChange={handleChange}
+        >
+          {agent.country.split(",").map((item) => (
+            <MenuItem key={item} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
