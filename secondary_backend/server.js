338633be98936eb4abc7cdfb618d71f148130cab
hadish <hadishhamraz@gmail.com>
2023-08-17 14:52:50 +0430

    starting to interact with sqlite db

--- secondary_backend/server.js
+++ secondary_backend/server.js
@@ -1,1 +1,52 @@
-// TEST
+const express = require('express');
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+app.use(express.json());
+
+const db = new sqlite3.Database('db.sqlite3', sqlite3.OPEN_READWRITE, (err) => 
+{
+    if (err) 
+    {
+        console.error(err.message);
+    } 
+    
+    else 
+    {
+        console.log('Connected to the database !');
+
+        const selectQuery = `SELECT * FROM users`;
+        db.all(selectQuery, [], (err, rows) => 
+        {
+            if (err) 
+            {
+                console.error(err.message);
+            } 
+            
+            else 
+            {
+                rows.forEach(row => 
+                {
+                    console.log(row);
+                });
+            }
+        });
+    }
+});
+
+
+app.post("/edit_expire_times", async (req, res) => 
+{
+
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(7002);
