7f3bf5b8188d2fbf4e2602f93d8ac1eb4c8f65f7
hadish <hadishhamraz@gmail.com>
2023-08-21 16:16:06 +0330

    added backup config

--- backup_service.js
+++ backup_service.js
@@ -2,54 +2,60 @@ const { Telegraf } = require('telegraf');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const axios = require('axios');
-const bot = new Telegraf('6598703756:AAHdCGAnBJYJ3-UKN76HKWFmF7XiqLfexas');bot.launch();
-const channelLink = "@asdar3refdsfdghyeffwwadf4fdgjkts";
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (s) => new Promise(resolve => setTimeout(resolve,s*1000));
 
 
-async function bu() 
+
+async function init() 
 {
+    var config_obj = JSON.parse(await fs.promises.readFile("./backup_config.json", "utf8"));
+
+    const bot = new Telegraf(config_obj.telegram.bot_token);bot.launch();
+    const chat_id = config_obj.telegram.chat_id;
+
+    const transporter = nodemailer.createTransport(config_obj.email.sender);           
+
+
+
+
     while(true)
     {
         try 
         {
           var res = (await axios.post("http://localhost:5000/dldb", { service_access_api_key : "resllmwriewfeujeh3i3ifdkmwheweljedifefhyr" })).data;
           var filePath = "./frontend/public" + res.split(">")[1];
-
-          // SEND IN TELEGRAM
-          await bot.telegram.sendDocument(channelLink, { source: filePath });
           var today = new Date();
           var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-          console.log(time + " ---> backup Sent to telegram");
-          // SEND IN EMAIL
-
-          const transporter = nodemailer.createTransport
-          ({
-            service: 'Gmail', 
-            auth: 
-            {
-              user: 'your_email@example.com', 
-              pass: 'your_email_password'
-            }
-          });
 
           const mailOptions = 
           {
-            from: 'your_email@example.com',
-            to: 'recipient@example.com',
-            subject: res.split("/dbdl/")[1],
+            from: config_obj.email.sender.auth.user,
+            to: config_obj.email.receiver,
+            subject: res.split("/dbdl/")[1].replace(".zip", ""),
             text: '',
             attachments: 
             [
               {
                 filename: 'db.zip',
-                path: "./frontend/public" + res.split(">")[1]
+                path: filePath
               }
             ]
           };
-          
-          await transporter.sendMail(mailOptions);
-          console.log(time + " ---> backup Sent to email");
+
+          // SEND IN TELEGRAM
+          if(!config_obj.telegram.disabled)
+          {
+            var res1 = await bot.telegram.sendDocument(chat_id, { source: filePath });
+            console.log(time + " ---> Backup Sent to telegram");
+            // console.log(res1);
+          }
+
+          // SEND IN EMAIL
+          if(!config_obj.email.disabled)
+          {
+            var res2 = await transporter.sendMail(mailOptions);
+            console.log(time + " ---> Backup Sent to email");
+          }
 
           await fs.promises.unlink(filePath);
         }
@@ -58,9 +64,9 @@ async function bu()
           console.log(err);
         }
 
-        await sleep(1800000);
+        await sleep(config_obj.interval);
     }
 }
 
-setTimeout(function(){bu();},2000);
+init();
 
