65b5da1769701a92a27fbb5261a2cfd16d8a00ff
SoheilKhorram <soheil13261428@gmail.com>
2023-11-09 11:02:45 +0330

    changing the xAxisDays

--- frontend/src/pages/admin/AdminHomePage.jsx
+++ frontend/src/pages/admin/AdminHomePage.jsx
@@ -42,100 +42,94 @@ const templateColor2 = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "
 const AdminHomePage = () => {
     const [error_msg, setError_msg] = useState("")
     const [hasError, setHasError] = useState(false)
-    const [xAxisDays1, setXAxisDays1] = useState(xAxisOptions[1]) 
+    const [xAxisDays1, setXAxisDays1] = useState(xAxisOptions[1])
     const [xAxisDays2, setXAxisDays2] = useState(xAxisOptions[1])
     const [xAxisDays3, setXAxisDays3] = useState(xAxisOptions[1])
-    const [total_users_creation, setTotal_users_creation] = useState([0,0,0,0,0,0,0]);
-    const [total_users_edit, setTotal_users_edit] = useState([0,0,0,0,0,0,0]);
-    const [total_users_delete, setTotal_users_delete] = useState([0,0,0,0,0,0,0]);
-    const [total_allocated_data_of_business_agents, setTotal_allocated_data_of_business_agents] = useState([0,0,0,0,0,0,0]);
-    const [total_data_usage_of_business_agents, setTotal_data_usage_of_business_agents] = useState([0,0,0,0,0,0,0]);
-    const [total_allocated_data_of_normal_agents, setTotal_allocated_data_of_normal_agents] = useState([0,0,0,0,0,0,0]);
-    const [total_data_usage_of_normal_agents, setTotal_data_usage_of_normal_agents] = useState([0,0,0,0,0,0,0]);
+    const [total_users_creation, setTotal_users_creation] = useState(new Array(30).fill(0))
+    const [total_users_edit, setTotal_users_edit] = useState(new Array(30).fill(0))
+    const [total_users_delete, setTotal_users_delete] = useState(new Array(30).fill(0))
+    const [total_allocated_data_of_business_agents, setTotal_allocated_data_of_business_agents] = useState([0, 0, 0, 0, 0, 0, 0])
+    const [total_data_usage_of_business_agents, setTotal_data_usage_of_business_agents] = useState([0, 0, 0, 0, 0, 0, 0])
+    const [total_allocated_data_of_normal_agents, setTotal_allocated_data_of_normal_agents] = useState([0, 0, 0, 0, 0, 0, 0])
+    const [total_data_usage_of_normal_agents, setTotal_data_usage_of_normal_agents] = useState([0, 0, 0, 0, 0, 0, 0])
 
-    var panels = JSON.parse(sessionStorage.getItem("panels"));
-    var agents = JSON.parse(sessionStorage.getItem("agents"));
+    var panels = JSON.parse(sessionStorage.getItem("panels"))
+    var agents = JSON.parse(sessionStorage.getItem("agents"))
 
 
     var home_data_obj =
     {
-        active_agents: [agents.filter(agent => agent.disable == "0").length , agents.length],
-        active_panels: [panels.filter(panel => panel.disable == "0").length , panels.length],
-        active_panel_users: [panels.reduce((acc, panel) => acc + panel.active_users,0) , panels.reduce((acc, panel) => acc + panel.total_users,0)],
-        active_agent_users: [agents.reduce((acc, panel) => acc + panel.active_users,0) , agents.reduce((acc, panel) => acc + panel.total_users,0)],
-        total_panel_usage: panels.reduce((acc, panel) => acc + panel.panel_data_usage,0),
-        total_agent_data_usage: agents.reduce((acc, agent) => acc + agent.used_traffic,0),
-    };
+        active_agents: [agents.filter(agent => agent.disable == "0").length, agents.length],
+        active_panels: [panels.filter(panel => panel.disable == "0").length, panels.length],
+        active_panel_users: [panels.reduce((acc, panel) => acc + panel.active_users, 0), panels.reduce((acc, panel) => acc + panel.total_users, 0)],
+        active_agent_users: [agents.reduce((acc, panel) => acc + panel.active_users, 0), agents.reduce((acc, panel) => acc + panel.total_users, 0)],
+        total_panel_usage: panels.reduce((acc, panel) => acc + panel.panel_data_usage, 0),
+        total_agent_data_usage: agents.reduce((acc, agent) => acc + agent.used_traffic, 0),
+    }
 
     const b2gb = (bytes) => {
-        var x = (bytes / (2 ** 10) ** 3);
-        return Math.round(x * 100) / 100;
+        var x = (bytes / (2 ** 10) ** 3)
+        return Math.round(x * 100) / 100
     }
 
-    const get_user_data = async (date_from,date_to) => 
-    {
-        var access_token = sessionStorage.getItem("access_token");
+    const get_user_data = async (date_from, date_to) => {
+        var access_token = sessionStorage.getItem("access_token")
 
-        const res = await axios.post("/graph/get_user_data", 
-        {
-            date_from,date_to,access_token
-        });
+        const res = await axios.post("/graph/get_user_data",
+            {
+                date_from, date_to, access_token
+            })
 
-        if (res.data.status === "ERR") 
-        {
+        if (res.data.status === "ERR") {
             setError_msg(res.data.msg)
             setHasError(true)
             return
         }
 
-        setTotal_users_creation(res.data.total_user_creation.map(x=>x.count));
-        setTotal_users_edit(res.data.total_user_edition.map(x=>x.count));
-        setTotal_users_delete(res.data.total_user_deletion.map(x=>x.count));
+        console.log(res.data)
+        setTotal_users_creation(res.data.total_user_creation.map(x => x.count))
+        setTotal_users_edit(res.data.total_user_edition.map(x => x.count))
+        setTotal_users_delete(res.data.total_user_deletion.map(x => x.count))
 
     }
 
-    const get_agent_data = async (date_from,date_to,business_mode) => 
-    {
-        var access_token = sessionStorage.getItem("access_token");
+    const get_agent_data = async (date_from, date_to, business_mode) => {
+        var access_token = sessionStorage.getItem("access_token")
 
-        const res = await axios.post("/graph/get_agent_data", 
-        {
-            date_from,date_to,access_token,business_mode
-        });
+        const res = await axios.post("/graph/get_agent_data",
+            {
+                date_from, date_to, access_token, business_mode
+            })
 
-        if (res.data.status === "ERR") 
-        {
+        if (res.data.status === "ERR") {
             setError_msg(res.data.msg)
             setHasError(true)
             return
         }
 
-        console.log(res.data);
-        if(business_mode==0)
-        {
-            setTotal_allocated_data_of_normal_agents(res.data.total_allocated_data.map(x=>x.volume))
-            setTotal_data_usage_of_normal_agents(res.data.total_data_usage.map(x=>b2gb(x.volume)))
+        console.log(res.data)
+        if (business_mode == 0) {
+            setTotal_allocated_data_of_normal_agents(res.data.total_allocated_data.map(x => x.volume))
+            setTotal_data_usage_of_normal_agents(res.data.total_data_usage.map(x => b2gb(x.volume)))
         }
 
-        else
-        {
-            setTotal_allocated_data_of_business_agents(res.data.total_allocated_data.map(x=>x.volume))
-            setTotal_data_usage_of_business_agents(res.data.total_data_usage.map(x=>b2gb(x.volume)))
+        else {
+            setTotal_allocated_data_of_business_agents(res.data.total_allocated_data.map(x => x.volume))
+            setTotal_data_usage_of_business_agents(res.data.total_data_usage.map(x => b2gb(x.volume)))
         }
     }
 
 
-    const get_date_range = (day_count) =>
-    {
-        var end_of_today = new Date();
-        end_of_today.setHours(23, 59, 59, 999);
-        var end_of_today_timestamp = Math.floor(end_of_today.getTime() / 1000);
+    const get_date_range = (day_count) => {
+        var end_of_today = new Date()
+        end_of_today.setHours(23, 59, 59, 999)
+        var end_of_today_timestamp = Math.floor(end_of_today.getTime() / 1000)
 
-        var start_date = new Date(Date.now()-(day_count-1)*24*60*60*1000)
-        start_date.setHours(0, 0, 0, 0);
-        var start_date_timestamp = Math.floor(start_date.getTime() / 1000);
+        var start_date = new Date(Date.now() - (day_count - 1) * 24 * 60 * 60 * 1000)
+        start_date.setHours(0, 0, 0, 0)
+        var start_date_timestamp = Math.floor(start_date.getTime() / 1000)
 
-        return [start_date_timestamp,end_of_today_timestamp];
+        return [start_date_timestamp, end_of_today_timestamp]
     }
 
 
@@ -154,9 +148,11 @@ const AdminHomePage = () => {
         return dateData
     }
 
-    const totalUserCreation = total_users_creation;
-    const totalUserEdits = total_users_edit
-    const totalUserDelete = total_users_delete
+    console.log(total_users_creation)
+    const totalUserCreation = total_users_creation.slice(total_users_creation.length - xAxisDays1.value)
+    const totalUserEdits = total_users_edit.slice(total_users_creation.length - xAxisDays1.value)
+    const totalUserDelete = total_users_delete.slice(total_users_creation.length - xAxisDays1.value)
+    console.log(totalUserCreation)
 
     const totalAllocatedDataOfBusinessAgents = total_allocated_data_of_business_agents
     const totalDataUsageOfBusinessAgents = total_data_usage_of_business_agents
@@ -180,47 +176,55 @@ const AdminHomePage = () => {
     const timeData2 = generateDateRange(daysPastFromToday2, xAxisDays2.value)
     const timeData3 = generateDateRange(daysPastFromToday3, xAxisDays3.value)
 
+    useEffect(() => {
+        var date_range = get_date_range(30)
+
+        get_user_data(date_range[0], date_range[1])
+
+        get_agent_data(date_range[0], date_range[1], 0)
+
+        get_agent_data(date_range[0], date_range[1], 1)
+    }, [])
 
 
-    useEffect(() => 
-    {
+    useEffect(() => {
 
-        var day_count = xAxisDays1.value;
+        var day_count = xAxisDays1.value
 
-        setTotal_users_creation(new Array(day_count).fill(0));
-        setTotal_users_edit(new Array(day_count).fill(0));
-        setTotal_users_delete(new Array(day_count).fill(0));
+        setTotal_users_creation(new Array(day_count).fill(0))
+        setTotal_users_edit(new Array(day_count).fill(0))
+        setTotal_users_delete(new Array(day_count).fill(0))
 
-        var date_range = get_date_range(day_count);
-        get_user_data(date_range[0],date_range[1]);
+        // var date_range = get_date_range(30)
+        // get_user_data(date_range[0], date_range[1])
 
     }, [xAxisDays1])
 
-    useEffect(() => 
-    {
+    useEffect(() => {
 
-        var day_count = xAxisDays3.value;
+        var day_count = xAxisDays3.value
 
         setTotal_allocated_data_of_normal_agents(new Array(day_count).fill(0))
         setTotal_data_usage_of_normal_agents(new Array(day_count).fill(0))
 
-        var date_range = get_date_range(day_count);
-        get_agent_data(date_range[0],date_range[1],0);
+        // var date_range = get_date_range(day_count)
+        // get_agent_data(date_range[0], date_range[1], 0)
 
     }, [xAxisDays3])
 
-    useEffect(() =>
-    {
-        var day_count = xAxisDays2.value;
+    useEffect(() => {
+        var day_count = xAxisDays2.value
 
         setTotal_allocated_data_of_business_agents(new Array(day_count).fill(0))
         setTotal_data_usage_of_business_agents(new Array(day_count).fill(0))
 
-        var date_range = get_date_range(day_count);
-        get_agent_data(date_range[0],date_range[1],1);
+        // var date_range = get_date_range(day_count)
+        // get_agent_data(date_range[0], date_range[1], 1)
 
     }, [xAxisDays2])
 
+    console.log(totalUserCreation)
+    console.log(total_users_creation)
     const config1 = {
         series: [
             { data: totalUserCreation },
@@ -244,6 +248,7 @@ const AdminHomePage = () => {
         height: 300,
     }
 
+    console.log(timeData1)
     const xAxisCommon1 = {
         data: [...timeData1],
         scaleType: 'time',
@@ -334,7 +339,7 @@ const AdminHomePage = () => {
                 </div>
             </div>
 
-            <div className='flex flex-col' style={{ marginTop: "1.25rem" }}>
+            {/* <div className='flex flex-col' style={{ marginTop: "1.25rem" }}>
                 <div style={{ alignSelf: "end" }} className='flex'>
                     <Dropdown options={xAxisOptions} value={xAxisDays2} onChange={setXAxisDays2} overlap={true} />
                 </div>
@@ -352,9 +357,9 @@ const AdminHomePage = () => {
                     <div className='flex gap-1' style={{ fontSize: ".875rem" }}><div style={{ backgroundColor: "#e41a1c", minWidth: ".875rem", height: ".875rem", borderRadius: ".25rem" }}></div>Total Allocated Data Of Business Agents</div>
                     <div className='flex gap-1' style={{ fontSize: ".875rem" }}><div style={{ backgroundColor: "#377eb8", minWidth: ".875rem", height: ".875rem", borderRadius: ".25rem" }}></div>Total Data Usage Of Business Agents</div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className='flex flex-col' style={{ marginTop: "1.25rem" }}>
+            {/* <div className='flex flex-col' style={{ marginTop: "1.25rem" }}>
                 <div style={{ alignSelf: "end" }} className='flex'>
                     <Dropdown options={xAxisOptions} value={xAxisDays3} onChange={setXAxisDays3} overlap={true} />
                 </div>
@@ -372,7 +377,7 @@ const AdminHomePage = () => {
                     <div className='flex gap-1' style={{ fontSize: ".875rem" }}><div style={{ backgroundColor: "#e41a1c", minWidth: ".875rem", height: ".875rem", borderRadius: ".25rem" }}></div>Total Allocated Data Of Normal Agents</div>
                     <div className='flex gap-1' style={{ fontSize: ".875rem" }}><div style={{ backgroundColor: "#377eb8", minWidth: ".875rem", height: ".875rem", borderRadius: ".25rem" }}></div>Total Data Usage Of Normal Agents</div>
                 </div>
-            </div>
+            </div> */}
 
             <ErrorCard
                 hasError={hasError}
