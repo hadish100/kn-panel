7da7a13718305311b86c28b21a31c3b2a9f089e4
pouya <hadishhamraz@gmail.com>
2024-10-16 11:52:01 +0330

    updated fix undefined

--- testing/fix_undefined.js
+++ testing/fix_undefined.js
@@ -5,31 +5,25 @@ var { users_clct } = require('./db_interface');
 async function init()
 {
     var db_users = await get_all_users();
-    // db_users = db_users.filter(u=>u.real_subscription_url.includes("undefined"));
+    db_users = db_users.filter(u=>u.real_subscription_url.includes("undefined"));
     console.log(`found ${db_users.length} users`);
 
     var panels = await get_panels();
 
-    for(panel of panels)
+    for(let panel of panels)
     {
         var panel_users = db_users.filter(u=>u.corresponding_panel_id == panel.id);
 
-        for(user of panel_users)
+        for(let user of panel_users)
         {
-            // var marzban_user = await get_marzban_user(panel.panel_url, panel.panel_username, panel.panel_password, user.username);
+            var marzban_user = await get_marzban_user(panel.panel_url, panel.panel_username, panel.panel_password, user.username);
+            console.log(marzban_user.subscription_url);
 
-            // if(marzban_user)
-            // {
-            //     await (await users_clct()).updateOne({id:user.id},{$set:{links:marzban_user.links}});
-            //     console.log(`user ${user.id} updated`);
-            // }
-
-            // replace https://cdx.irtunir.com:9000 with https://cdx.irtunir.com in every user
-            await (await users_clct()).updateOne({id:user.id},{$set:{subscription_url:user.subscription_url.replace("https://cdx.irtunir.com:9000","https://cdx.irtunir.com")}});
-            console.log("old url: " + user.subscription_url);
-            console.log("new url: " + user.subscription_url.replace("https://cdx.irtunir.com:9000","https://cdx.irtunir.com"));
-            console.log("====================================");
-            // console.log(panel.panel_url.replace(/http(?![s])/,"https") + "/sub/" + user.real_subscription_url.split("/sub/")[1]);
+            if(marzban_user)
+            {
+                // await (await users_clct()).updateOne({id:user.id},{$set:{links:marzban_user.links}});
+                console.log(`user ${user.id} updated`);
+            }
         }
     }
 
