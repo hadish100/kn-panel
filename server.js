19e676eefcc742dc5ad811784836eeef2cfb0a2c
pouya <hadishhamraz@gmail.com>
2025-04-08 11:47:56 +0330

    bl extension

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var AdmZip = require("adm-zip");
 var { accounts_clct, panels_clct, users_clct, logs_clct } = require('./db_interface');
 const amnezia_sub_page_html = fs.readFileSync("custom_sub/amnezia.html").toString();
 const not_found_page_html = fs.readFileSync("custom_sub/404.html").toString();
-const AMNEZIA_COEFFICIENT = 2;
+var AMNEZIA_COEFFICIENT = 2;
+if(process.env.RELEASE == "ALI") AMNEZIA_COEFFICIENT = 10;
 var SD_VARIABLE = 0;
 
 const { 
